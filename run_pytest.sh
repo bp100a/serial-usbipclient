1f907f1a4001fcd2335596da3235e21ffc774dd5
Harry <hcollins@sunriselabs.com>
2024-07-30 08:43:54 -0400

    no need to make folder for .coverage

--- run_pytest.sh
+++ run_pytest.sh
@@ -4,6 +4,5 @@ export PYTHONPATH=./
 PYTEST_ARGS=(-n auto -v --timeout=30 --cov --cov-branch --cov-config=.coveragerc)
 pytest "${PYTEST_ARGS[@]}"
 pytest_exit_code=$?
-mkdir -p ./.coverage
 coverage json -o .coverage/coverage-summary.json
 exit "$pytest_exit_code"
