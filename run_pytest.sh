85a2c04bd6c80e6aa796e62d1175f898963951fb
Harry <hcollins@sunriselabs.com>
2024-07-30 08:45:45 -0400

    display folder contents

--- run_pytest.sh
+++ run_pytest.sh
@@ -4,5 +4,7 @@ export PYTHONPATH=./
 PYTEST_ARGS=(-n auto -v --timeout=30 --cov --cov-branch --cov-config=.coveragerc)
 pytest "${PYTEST_ARGS[@]}"
 pytest_exit_code=$?
+# see what's in the folder
+ls -als
 coverage json -o .coverage/coverage-summary.json
 exit "$pytest_exit_code"
