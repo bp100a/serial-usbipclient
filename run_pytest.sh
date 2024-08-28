950704cea447a2f63409781551a8c441d73113f3
Harry <hcollins@sunriselabs.com>
2024-08-28 09:27:54 -0400

    use poetry to launch pylint & pytest

--- run_pytest.sh
+++ run_pytest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # run pytest
-source ./venv/Scripts/activate
 export PYTHONPATH=./
 PYTEST_ARGS=(-n auto -v --timeout=30 --cov --cov-branch --cov-config=.coveragerc)
-pytest "${PYTEST_ARGS[@]}"
+python -m poetry run python -m pytest "${PYTEST_ARGS[@]}"
