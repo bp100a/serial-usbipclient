6bcc21e028b624afdbc402cef9c0c83da5e12728
Harry <hcollins@sunriselabs.com>
2024-08-28 09:24:03 -0400

    activate the venv

--- run_pytest.sh
+++ run_pytest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # run pytest
+source ./venv/Scripts/activate
 export PYTHONPATH=./
 PYTEST_ARGS=(-n auto -v --timeout=30 --cov --cov-branch --cov-config=.coveragerc)
 pytest "${PYTEST_ARGS[@]}"
